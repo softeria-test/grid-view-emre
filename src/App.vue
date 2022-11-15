4b24bf13022c978b114317d20ce874a73501f99d
emreliman <emrekulaksiz1999@gmail.com>
2022-11-15 12:18:10 +0000

    tests added some yarn warnings fixed convert script setup to class type

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div id="app">
-    <GridTable />
+    <GridTable :table="table"/>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { ref } from "vue";
 import GridTable from "./components/GridTable.vue"; // NOSONAR
+import stach from "./stach-sdk";
+type IRow =
+  | stach.factset.protobuf.stach.v2.RowOrganizedPackage.IRow[]
+  | null
+  | undefined;
+const table = ref<IRow>();
+// fetch data from the server
+fetch("http://localhost:3000/data")
+  .then((response) => response.json())
+  .then((data) => {
+    const pkg =
+      stach.factset.protobuf.stach.v2.RowOrganizedPackage.create(data);
+    table.value = pkg.tables.main.data?.rows;
+  });
 
 </script>
+
 
 <style lang="scss">
 #app {
@@ -19,4 +34,5 @@ import GridTable from "./components/GridTable.vue"; // NOSONAR
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
