1e0931d2c932da263005d379ea25fec5c1c6761b
emreliman <emrekulaksiz1999@gmail.com>
2022-12-13 22:10:41 +0000

    childLength added

--- src/App.vue
+++ src/App.vue
@@ -20,9 +20,29 @@ fetch("http://localhost:3000/data")
     const pkg =
       stach.factset.protobuf.stach.v2.RowOrganizedPackage.create(data);
     table.value = pkg.tables.main.data?.rows;
+    //add isOpen every element of table
     table.value?.forEach((item:any)=>{
       item["isOpen"] = false
     })
+    //add childlength to each row according to grouplevel property 
+    table.value?.forEach((item:any,index:number)=>{
+      item["childLength"] = 0
+      if(!item.cellDetails?.[0].groupLevel){
+
+        for(let i = index+1;i<table.value.length;i++){
+          if(table.value[i].cellDetails?.[0].groupLevel){
+            if(table.value[i].cellDetails?.[0].groupLevel === 1)
+          item["childLength"] += 1
+          }
+          else{
+            break
+          }
+        }
+      }
+      
+    })
+    
+
     console.log(table.value);
   });
 
